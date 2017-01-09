9c2db0aa3ccd06b804a4f91b69514bdd76a2c1f5
Mat <fukano.mat@gmail.com>
2017-01-09 10:58:46 -0800

    Update config

--- wp-config.php
+++ wp-config.php
@@ -81,6 +81,8 @@ $table_prefix  = 'wp_';
  */
 define('WP_DEBUG', false);
 
+define('WP_ALLOW_REPAIR', true);
+
 /* That's all, stop editing! Happy blogging. */
 
 /** Absolute path to the WordPress directory. */
